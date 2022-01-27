03c06fe06a49254441ebc95091947326d05ec7d7
BhumiRaval12 <bhumir@zignuts.com>
2022-01-27 19:15:46 +0530

    updated file

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var todoControllers = require('./controllers/todoController');
+
+var app = express();
+
+
+//set up template engine
+
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire function
+todoControllers(app);
+
+//listen to port
+app.listen(8080);
+console.log('you are listening to port...8080');
